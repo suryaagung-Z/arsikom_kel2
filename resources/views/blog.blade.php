03031c584e3d9e99db677cb26fbbe3d75c76bc31
Rajhaga <hagaraj7@gmail.com>
2024-05-06 13:29:43 +0700

    view,asset,web

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,36 @@
+@extends('Navbar.navbar')
+
+@section('main')
+<!-- /End Fables Navigation -->  
+
+<div class="container py-4 py-lg-5"> 
+    <div class="fables-blog my-3">
+       <h2 class="fables-second-text-color mb-4 mb-lg-5 font-weight-bold">Blog</h2> 
+       <div class="row"> 
+           <div class="col-12 col-md-4 mb-4 mb-lg-5">  
+                  <a href="/blogdetail"><img src="assets/custom/images/magazine-2559842.jpg" alt="" class="w-100"></a> 
+                  <div class="above-date py-3 fables-fifth-text-color">
+                      <div class="float-left font-13">
+                             <span class="fables-iconcalender"></span> December 27, 2017  
+                       </div> 
+                       <div class="float-right font-13">
+                           <span class="fables-iconnews"></span> Latest News 
+                       </div>
+                  </div>
+                  <h2 class="font-weight-bold font-20 my-3"><a href="/blogdetail" class="fables-second-text-color fables-main-hover-color">Grow Your Business</a></h2>
+
+                  <p class="fables-forth-text-color font-14">
+                      When you need your company to have a new  website or if you venture on updating your old webpage with a new look and functionality. the
+                  </p>
+                  <a href="/blogdetail" class="btn fables-second-text-color fables-main-hover-color p-0 mt-2">
+                      <span class="underline">Read More</span>
+                      <span class="fables-iconarrow-light ml-2"></span>
+                  </a> 
+
+          </div>
+       </div>  
+      
+ 
+    </div> 
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome', ["products" => Product::all()]);
 });
+
+Route::get('/', function () {
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/blog', function () {
+    return view('blog');
+});
+
+Route::get('/blogdetail', function () {
+    return view('blog-details');
+});
