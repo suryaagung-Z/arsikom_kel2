213b95030d5a8246083282bdeba1c232239f2933
rajhaga <Hagaraj7@gmail.com>
2024-06-03 16:04:33 +0700

    update laptop

--- database/migrations/2024_04_18_134207_create_products_table.php
+++ database/migrations/2024_04_18_134207_create_products_table.php
@@ -28,3 +28,46 @@ return new class extends Migration
         Schema::dropIfExists('products');
     }
 };
+
+
+// use Illuminate\Database\Migrations\Migration;
+// use Illuminate\Database\Schema\Blueprint;
+// use Illuminate\Support\Facades\Schema;
+
+// class CreateLaptopsTable extends Migration
+// {
+//     /**
+//      * Run the migrations.
+//      *
+//      * @return void
+//      */
+//     public function up()
+//     {
+//         Schema::create('laptops', function (Blueprint $table) {
+//             $table->id();
+//             $table->string('name');
+//             $table->text('description');
+//             $table->string('processor');
+//             $table->string('ram');
+//             $table->string('storage');
+//             $table->string('graphics');
+//             $table->string('display');
+//             $table->string('operating_system');
+//             $table->string('connectivity');
+//             $table->text('ports');
+//             $table->string('battery');
+//             $table->string('image')->nullable();
+//             $table->timestamps();
+//         });
+//     }
+
+//     /**
+//      * Reverse the migrations.
+//      *
+//      * @return void
+//      */
+//     public function down()
+//     {
+//         Schema::dropIfExists('laptops');
+//     }
+// }
